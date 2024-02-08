06f7a67a2aa4912dba5f1bddc38f73e97b31213d
rodis2003 <rodisenrique73@gmail.com>
2024-02-07 20:41:23 -0500

    spring initializr

---
+++ src/main/java/com/rersdev/gestioncontactos/GestionContactosApplication.java
@@ -0,0 +1,13 @@
+package com.rersdev.gestioncontactos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionContactosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionContactosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rersdev/gestioncontactos/GestionContactosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rersdev.gestioncontactos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionContactosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
