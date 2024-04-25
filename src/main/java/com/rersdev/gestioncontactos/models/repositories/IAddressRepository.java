1d895b0fa6c2419745b40116722c278de8e5a48d
rodis2003 <rodisenrique73@gmail.com>
2024-04-25 12:27:11 -0500

    chore: rename repositories

--- src/main/java/com/rersdev/gestioncontactos/models/repositories/IAddressRepository.java
+++ src/main/java/com/rersdev/gestioncontactos/models/repositories/IAddressRepository.java
@@ -1,11 +1,11 @@
 package com.rersdev.gestioncontactos.models.repositories;
 
-import com.rersdev.gestioncontactos.models.entities.Phone;
+import com.rersdev.gestioncontactos.models.entities.Address;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface PhoneRepository extends CrudRepository<Phone, UUID> {
+public interface IAddressRepository extends CrudRepository<Address, UUID> {
 }
--- src/main/java/com/rersdev/gestioncontactos/models/repositories/IPhoneRepository.java
+++ src/main/java/com/rersdev/gestioncontactos/models/repositories/IPhoneRepository.java
@@ -1,11 +1,11 @@
 package com.rersdev.gestioncontactos.models.repositories;
 
-import com.rersdev.gestioncontactos.models.entities.Address;
+import com.rersdev.gestioncontactos.models.entities.Phone;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface AddressRepository extends CrudRepository<Address, UUID> {
+public interface IPhoneRepository extends CrudRepository<Phone, UUID> {
 }
