1981ca76ec5c87ac867c57b3ba7dc6432f9ca02f
rers <rodisenrique73@gmail.com>
2024-07-11 11:14:19 -0500

    feat: add mapper to update contact

--- src/main/java/com/rersdev/gestioncontactos/controllers/mappers/IContactMapper.java
+++ src/main/java/com/rersdev/gestioncontactos/controllers/mappers/IContactMapper.java
@@ -5,6 +5,7 @@ import com.rersdev.gestioncontactos.controllers.DTO.ShowContactDataDTO;
 import com.rersdev.gestioncontactos.models.entities.Contact;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface IContactMapper {
@@ -12,4 +13,8 @@ public interface IContactMapper {
     Contact toEntity(InsertContactDataDTO insertContactDataDTO);
 
     ShowContactDataDTO toDTO(Contact contact);
+
+    Contact toEntity(ShowContactDataDTO showContactDataDTO);
+
+    void updateContactFromDTO(@MappingTarget Contact contact, InsertContactDataDTO updatedContact);
 }
