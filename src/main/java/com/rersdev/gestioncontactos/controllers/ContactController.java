c7a820460d1ad49fb691c8b6e270703b2e0c2e77
rers <rodisenrique73@gmail.com>
2024-07-11 10:17:50 -0500

    feat: add endpoint to get all contacts

--- src/main/java/com/rersdev/gestioncontactos/controllers/ContactController.java
+++ src/main/java/com/rersdev/gestioncontactos/controllers/ContactController.java
@@ -1,8 +1,10 @@
 package com.rersdev.gestioncontactos.controllers;
 
 import com.rersdev.gestioncontactos.controllers.DTO.InsertContactDataDTO;
+import com.rersdev.gestioncontactos.controllers.DTO.ShowContactDataDTO;
 import com.rersdev.gestioncontactos.services.IContactService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,5 +25,11 @@ public class ContactController {
         return ResponseEntity
                 .created(new URI("api/v1/contacts"))
                 .body("Contact registered successfully");
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<ShowContactDataDTO>> getAllContacts(@RequestParam(value = "page", defaultValue = "0") int page,
+                                                                   @RequestParam(value = "size", defaultValue = "5") int size){
+        return ResponseEntity.ok().body(contactService.getAllContacts(page, size));
     }
 }
