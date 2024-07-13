72c395aba14525932504b46dddca059db05fc116
rers <rodisenrique73@gmail.com>
2024-07-12 19:37:46 -0500

    feat: add endpoints to upload contact image and get contact image

--- src/main/java/com/rersdev/gestioncontactos/controllers/ContactController.java
+++ src/main/java/com/rersdev/gestioncontactos/controllers/ContactController.java
@@ -1,24 +1,37 @@
 package com.rersdev.gestioncontactos.controllers;
 
+import com.rersdev.gestioncontactos.configuration.util.LocalDirectory;
 import com.rersdev.gestioncontactos.controllers.DTO.InsertContactDataDTO;
 import com.rersdev.gestioncontactos.controllers.DTO.ShowContactDataDTO;
 import com.rersdev.gestioncontactos.services.IContactService;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.UUID;
 
-@RequiredArgsConstructor
+import static org.springframework.util.MimeTypeUtils.IMAGE_JPEG_VALUE;
+import static org.springframework.util.MimeTypeUtils.IMAGE_PNG_VALUE;
 
 @RestController
 @RequestMapping("/contacts")
 public class ContactController {
 
     private final IContactService contactService;
+    private final String imageDirectory;
+
+    @Autowired
+    public ContactController(IContactService contactService) {
+        this.contactService = contactService;
+        this.imageDirectory = LocalDirectory.getImageDirectory();
+    }
 
     @PostMapping
     public ResponseEntity<String> registerContact(@RequestBody InsertContactDataDTO contactDataDTO) throws URISyntaxException {
@@ -34,14 +47,24 @@ public class ContactController {
         return ResponseEntity.ok().body(contactService.getAllContacts(page, size));
     }
 
-    @GetMapping("/{id} ")
-    public ResponseEntity<ShowContactDataDTO> getContactById(@PathVariable("id") UUID id){
+    @GetMapping("/{id}")
+    public ResponseEntity<ShowContactDataDTO> getContactById(@PathVariable UUID id){
         return ResponseEntity.ok().body(contactService.getContactById(id));
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<ShowContactDataDTO> updateContact(@PathVariable("id") UUID id,
+    public ResponseEntity<ShowContactDataDTO> updateContact(@PathVariable UUID id,
                                                             @RequestBody InsertContactDataDTO updatedContact){
         return ResponseEntity.ok().body(contactService.updateContact(updatedContact, id));
+    }
+
+    @PutMapping("/image")
+    public ResponseEntity<String> uploadImage(@RequestParam UUID id, @RequestParam MultipartFile file){
+        return ResponseEntity.ok().body(contactService.uploadImage(file, id));
+    }
+
+    @GetMapping(path = "image/{filename}", produces = {IMAGE_PNG_VALUE,IMAGE_JPEG_VALUE})
+    public byte[] getImage(@PathVariable("filename") String filename) throws IOException {
+        return Files.readAllBytes(Paths.get(imageDirectory + filename));
     }
 }
