2d4f0db4ab91cc6ab18b04825ff485f747f35af5
rers <rodisenrique73@gmail.com>
2024-07-12 19:39:53 -0500

    feat: add service to upload contact image

--- src/main/java/com/rersdev/gestioncontactos/services/IContactService.java
+++ src/main/java/com/rersdev/gestioncontactos/services/IContactService.java
@@ -3,6 +3,7 @@ package com.rersdev.gestioncontactos.services;
 import com.rersdev.gestioncontactos.controllers.DTO.InsertContactDataDTO;
 import com.rersdev.gestioncontactos.controllers.DTO.ShowContactDataDTO;
 import org.springframework.data.domain.Page;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.UUID;
 
@@ -17,4 +18,6 @@ public interface IContactService {
     Page<ShowContactDataDTO> getAllContacts(int page, int size);
 
     void deleteContact(UUID id);
+
+    String uploadImage(MultipartFile file, UUID id);
 }
