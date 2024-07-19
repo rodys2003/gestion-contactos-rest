a841cc737d294fccc0f1c6938398d64da7f6c269
rers <rodisenrique73@gmail.com>
2024-07-18 20:11:24 -0500

    refactor: improve regular phone expression

--- src/main/java/com/rersdev/gestioncontactos/controllers/dto/InsertContactDataDTO.java
+++ src/main/java/com/rersdev/gestioncontactos/controllers/dto/InsertContactDataDTO.java
@@ -9,28 +9,26 @@ import java.io.Serializable;
 public record InsertContactDataDTO(
 
         @NotBlank(message = "Contact name is required")
-        @Size(min = 2, message = "The name must be at least 2 characters long.")
-        @Size(max = 30, message = "The name cannot exceed 30 characters.")
+        @Size(min = 2, message = "The name must be at least 2 characters long")
+        @Size(max = 30, message = "The name cannot exceed 30 characters")
         String name,
 
-        @Size(min = 5, message = "The title must be at least 10 characters long.")
-        @Size(max = 30, message = "The title cannot exceed 30 characters.")
+        @Size(min = 5, message = "The title must be at least 10 characters long")
+        @Size(max = 30, message = "The title cannot exceed 30 characters")
         String title,
 
         @Pattern(regexp = "(?i)^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,6}+$", message = "Invalid email")
         String email,
 
         @NotBlank(message = "Phone number is required")
-        @Size(min = 10, message = "The phone number must be at least 10 characters long.")
-        @Size(max = 14, message = "The phone cannot exceed 14 characters.")
-        @Pattern(regexp = "^(\\+)?\\d+$", message = "The phone number only accept numbers")
+        @Pattern(regexp = "^(\\+)?\\d{10,13}$", message = "Phone number must be between 10 and 13 digits")
         String phone,
 
-        @Size(min = 15, message = "The address must be at least 15 characters long.")
-        @Size(max = 50, message = "The address cannot exceed 50 characters.")
+        @Size(min = 10, message = "The address must be at least 10 characters long")
+        @Size(max = 40, message = "The address cannot exceed 40 characters")
         String address,
 
-        @Size(min = 5, max = 15, message = "It must be at least 5 characters long and it cannot exceed 15 characters.")
+        @Size(min = 5, max = 15, message = "It must be at least 5 characters long and it cannot exceed 15 characters")
         String status
 
 ) implements Serializable {
